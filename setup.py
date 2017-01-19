d1719864f4256fe03a261caa59a778ff648715f9
fabiobatalha <fabiobatalha@gmail.com>
2017-01-19 11:26:51 -0200

    Ajustes em estrutura de diretórios

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+install_requires = [
+    'articlemetaapi==1.6.15',
+    'citedbyapi==1.3.10',
+    'requests==2.11.1',
+    'lxml-=3.7.2',
+    'picles.plumber==0.10',
+    'solrapi'
+]
+
+tests_require = []
+
+setup(
+    name="UpdateSearch",
+    version='1.2.2',
+    description="Process article to Solr",
+    author="SciELO",
+    author_email="scielo-dev@googlegroups.com",
+    license="BSD",
+    url="https://github.com/scieloorg/search-journals-proc",
+    keywords='search scielo',
+    maintainer_email='atta.jamil@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        "Topic :: System",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Operating System :: POSIX :: Linux",
+    ],
+    dependency_links=[
+        "git+https://github.com/picleslivre/solrapi#egg=solrapi"
+    ],
+    test_suite='tests',
+    tests_require=tests_require,
+    install_requires=install_requires,
+    entry_points="""
+    [console_scripts]
+    update_search=updatesearch.dumpdata:main
+    """
+)
