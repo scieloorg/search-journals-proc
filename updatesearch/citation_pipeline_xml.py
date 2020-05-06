24fb8ecb7dfd8be1930f90863cc677f12ac9b540
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-05-06 12:04:22 -0300

    Adiciona Pipe MonographiAuthors para referências citadas

--- updatesearch/citation_pipeline_xml.py
+++ updatesearch/citation_pipeline_xml.py
@@ -375,3 +375,37 @@ class Issue(plumber.Pipe):
         return data
 
 
+class MonographicAuthors(plumber.Pipe):
+
+    def precond(data):
+
+        raw, xml = data
+
+        if not raw.monographic_authors:
+            raise plumber.UnmetPrecondition()
+
+    @plumber.precondition(precond)
+    def transform(self, data):
+        raw, xml = data
+
+        for author in raw.monographic_authors:
+            field = ET.Element('field')
+            name = []
+
+            if 'surname' in author:
+                name.append(author['surname'])
+
+            if 'given_names' in author:
+                name.append(author['given_names'])
+
+            fullname = ', '.join(name)
+            cleaned_fullname = fs.remove_endpoint(fullname)
+
+            if cleaned_fullname:
+                field.text = cleaned_fullname
+                field.set('name', 'cit_mon_au')
+                xml.find('.').append(field)
+
+        return data
+
+
