3d4e4a9137ac4d33bd80c092d1d7864d59ca5cfb
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-05-06 12:02:49 -0300

    Adiciona Pipe IndexNumber para referências citadas

--- updatesearch/citation_pipeline_xml.py
+++ updatesearch/citation_pipeline_xml.py
@@ -269,3 +269,17 @@ class ExternalMetaData(plumber.Pipe):
         return data
 
 
+class IndexNumber(plumber.Pipe):
+
+    def transform(self, data):
+        raw, xml = data
+
+        field = ET.Element('field')
+        field.text = str(raw.index_number)
+        field.set('name', 'cit_index_number')
+
+        xml.find('.').append(field)
+
+        return data
+
+
