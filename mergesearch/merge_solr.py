ad433c222517c9c5a036c0127db681c95e106c69
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-07-10 12:22:23 -0300

    Adiciona métodos para persistir dados

--- mergesearch/merge_solr.py
+++ mergesearch/merge_solr.py
@@ -24,3 +24,28 @@ class MergeSolr(object):
         self.mongo = mongo
         self.persist_on_solr = persist_on_solr
 
+    def dump_data(self, data, filename):
+        """
+        Persiste dados no disco, em formato JSON
+
+        :param data: Dados a serem persistidos
+        :param filename: Nome do arquivo JSON
+        """
+        str_time = datetime.utcnow().isoformat(sep='_', timespec='milliseconds')
+        filepath = '-'.join([self.cit_hash_base, filename, str_time]) + '.json'
+
+        with open(os.path.join('merges', filepath), 'w') as f:
+            f.write(data)
+
+    def persist(self, data, data_name):
+        """
+        Persiste data no disco e no Solr, se indicado.
+
+        :param data: Dados a serem persistidos
+        :param data_name: Nome do conjunto de dados a ser persistido
+        """
+        self.dump_data(str(data), data_name)
+
+        if self.persist_on_solr:
+            self.solr.update(str(data).encode('utf-8'), headers={'content-type': 'application/json'})
+
