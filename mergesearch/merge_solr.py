dd1b0e39c37a1b5ef0df54e9c4de57f0cbff7cb2
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-07-10 12:24:33 -0300

    Adiciona método para obter ids de citações a serem mescladas

--- mergesearch/merge_solr.py
+++ mergesearch/merge_solr.py
@@ -37,6 +37,42 @@ class MergeSolr(object):
         with open(os.path.join('merges', filepath), 'w') as f:
             f.write(data)
 
+    def get_ids_for_merging(self, mongo_filter: dict):
+        """
+        Coleta identificadores da base de de-duplicação.
+
+        :param mongo_filter: Filtro Mongo para selecionar parte dos identificadores
+        :return: Identificadores hash das citações a serem mescladas
+        """
+        logging.info('Getting cited references identifiers... [filter=%s]' % mongo_filter)
+
+        ids_for_merging = []
+
+        for j in self.mongo.find(mongo_filter):
+
+            item = {
+                '_id': j['_id'],
+                'cit_full_ids': j['cit_full_ids'],
+                'citing_docs': j['citing_docs']
+            }
+
+            # Caso base de de-duplicação seja issue, mantem na citação principal o melhor valor de issue
+            if self.cit_hash_base == 'article_issue':
+                item.update({'cit_issue': j['cit_keys']['cleaned_issue']})
+
+            # Caso base de de-duplicação seja start_page, mantem na citação principal o melhor valor de start_page
+            if self.cit_hash_base == 'article_start_page':
+                item.update({'cit_start_page': j['cit_keys']['cleaned_start_page']})
+
+            # Caso base de de-duplicação seja volume, mantem na citação principal o melhor valor de volume
+            if self.cit_hash_base == 'article_volume':
+                item.update({'cit_volume': j['cit_keys']['cleaned_volume']})
+
+            ids_for_merging.append(item)
+
+        logging.info('There are %d cited references identifiers to be merged.' % len(ids_for_merging))
+
+        return ids_for_merging
     def persist(self, data, data_name):
         """
         Persiste data no disco e no Solr, se indicado.
